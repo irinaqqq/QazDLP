a9d5a251247beea15e2203dd12e4d4ddef7b5217
irinaqqq <samataskarbaiev@gmail.com>
2023-06-04 19:47:29 +0600

    some changes added

--- masker/utils.py
+++ masker/utils.py
@@ -1,20 +1,124 @@
 import re
 
+
+def generate_shift(keyword):
+    # Generate the shift amount based on the letters of the keyword
+    # For example, if the keyword is "secret", the shift would be 18-4-2-17-4-19 = -10
+    shift = sum(ord(char) - 97 for char in keyword.lower())
+    return shift
+
+keyword = "secret123"
+shift = generate_shift(keyword)
+dshift = - shift
+
+
+email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+phone_regex = r'\b\d{11}\b'
+name_regex = r'\b([А-ЯЁ][а-яё]+)\b'
+surname_regex = r'\b([А-ЯЁ][а-яё]+(-[А-ЯЁ][а-яё]+)?)\b'
+codes_regex = r'\b\d{9}\b' 
+
 def mask_text(text):
-    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    phone_regex = r'\b\d{11}\b' 
-    name_regex = r'\b([А-ЯЁ][а-яё]+)\b'
-    surname_regex = r'\b([А-ЯЁ][а-яё]+(-[А-ЯЁ][а-яё]+)?)\b'
-    codes_regex = r'\b\d{9}\b' 
-    month_regex = r'\b(январь|февраль|март|апрель|май|июнь|июль|август|сентябрь|октябрь|ноябрь|декабрь|қаңтарда|ақпанда|наурызда|сәуірде|мамырда|маусымда|шілде|тамызда|қыркүйекте|қазанда|қарашада|желтоқсанда)\b'
     def mask(match):
-        return '*' * len(match.group(0))
+        plaintext = match.group(0)
+        if re.match(email_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isalpha():
+                    shifted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+                else:
+                    shifted_char = char
+                ciphertext += shifted_char
+        elif re.match(phone_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isdigit():
+                    shifted_digit = str((int(char) + shift) % 10)
+                else:
+                    shifted_digit = char
+                ciphertext += shifted_digit
+        elif re.match(name_regex, plaintext):
+            plaintext = plaintext.lower()
+            ciphertext = ''
+            for i, char in enumerate(plaintext):
+                if char.isalpha():
+                    shifted_char = chr((ord(char) - ord('а') + shift) % 26 + ord('а'))
+                    if i == 0:
+                        ciphertext += shifted_char.upper()
+                    else:
+                        ciphertext += shifted_char
+                else:
+                    ciphertext += char
+        elif re.match(codes_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isdigit():
+                    shifted_digit = str((int(char) + shift) % 10)
+                else:
+                    shifted_digit = char
+                ciphertext += shifted_digit
+        else:
+            ciphertext = '*' * len(plaintext)
+        return ciphertext
 
     masked_text = re.sub(email_regex, mask, text)
     masked_text = re.sub(phone_regex, mask, masked_text)
     masked_text = re.sub(name_regex, mask, masked_text)
     masked_text = re.sub(surname_regex, mask, masked_text)
-    masked_text = re.sub(month_regex, mask, masked_text)
+    masked_text = re.sub(codes_regex, mask, masked_text)
+
+    return masked_text
+
+
+
+def unmask_text(text):
+    def mask(match):
+        plaintext = match.group(0)
+        if re.match(email_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isalpha():
+                    shifted_char = chr((ord(char) - ord('a') + dshift) % 26 + ord('a'))
+                else:
+                    shifted_char = char
+                ciphertext += shifted_char
+                continue
+        elif re.match(phone_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isdigit():
+                    shifted_digit = str((int(char) + dshift) % 10)
+                else:
+                    shifted_digit = char
+                ciphertext += shifted_digit
+        elif re.match(name_regex, plaintext):
+            ciphertext = ''
+            plaintext = plaintext.lower()
+            for i, char in enumerate(plaintext):
+                if char.isalpha():
+                    shifted_char = chr((ord(char) - ord('а') + dshift) % 26 + ord('а'))
+                    if i == 0:
+                        ciphertext += shifted_char.upper()
+                    else:
+                        ciphertext += shifted_char
+                else:
+                    ciphertext += char
+        elif re.match(codes_regex, plaintext):
+            ciphertext = ''
+            for char in plaintext:
+                if char.isdigit():
+                    shifted_digit = str((int(char) + dshift) % 10)
+                else:
+                    shifted_digit = char
+                ciphertext += shifted_digit
+        else:
+            ciphertext = '*' * len(plaintext)
+        return ciphertext
+
+    masked_text = re.sub(email_regex, mask, text)
+    masked_text = re.sub(phone_regex, mask, masked_text)
+    masked_text = re.sub(name_regex, mask, masked_text)
+    masked_text = re.sub(surname_regex, mask, masked_text)
     masked_text = re.sub(codes_regex, mask, masked_text)
 
     return masked_text
--- masker/views.py
+++ masker/views.py
@@ -1,82 +1,81 @@
 import io
 import os
-
 import docx
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import MaskerForm
-from .utils import mask_text
+from .utils import *
 
 
 def masker(request):
     if request.method == 'POST':
+        encryption_type = request.POST.get('encryption_type')
         form = MaskerForm(request.POST, request.FILES)
         if form.is_valid():
             text = form.cleaned_data['text']
             file = request.FILES.get('file', None)
 
-            if file:
-                # If a file was uploaded, read its contents and mask the text
-                fs = FileSystemStorage()
-                filename = fs.save(file.name, file)
-                filepath = os.path.join(settings.MEDIA_ROOT, filename)
+            if encryption_type == 'file':
+                if file:
+                    _, ext = os.path.splitext(file.name)
+                    if ext == '.txt':
+                        file_text = file.read().decode('utf-8')
+                        file_text = mask_text(file_text)
+                        response = HttpResponse(file_text, content_type='text/plain')
+                        response['Content-Disposition'] = 'attachment; filename="masked_file.txt"'
+                    elif ext == '.docx':
+                        doc = docx.Document(file)
+                        for para in doc.paragraphs:
+                            para.text = mask_text(para.text)
+                        output = io.BytesIO()
+                        doc.save(output)
+                        output.seek(0)
+                        response = HttpResponse(output.read(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                        response['Content-Disposition'] = 'attachment; filename="masked_file.docx"'
+                    else:
+                        return HttpResponse('Unsupported file type')
 
-                # Determine the file extension and read the file contents
-                _, ext = os.path.splitext(filepath)
-                if ext == '.txt':
-                    with open(filepath, 'r', encoding='utf-8') as f:
-                        file_text = f.read()
-
-                    # Mask the file text
-                    file_text = mask_text(file_text)
-                elif ext == '.docx':
-                    # Load the DOC file using python-docx
-                    doc = docx.Document(filepath)
-
-                    # Iterate over all paragraphs in the document
-                    for para in doc.paragraphs:
-                        # Mask the text in each paragraph
-                        para.text = mask_text(para.text)
-
-                    # Iterate over all tables in the document
-                    for table in doc.tables:
-                        # Iterate over all cells in each table
-                        for row in table.rows:
-                            for cell in row.cells:
-                                # Mask the text in each cell
-                                cell.text = mask_text(cell.text)
-
-                    # Save the modified document
-                    doc.save(filepath)
-
-                    # Read the modified file contents
-                    with open(filepath, 'rb') as f:
-                        file_text = f.read()
+                    return response
                 else:
-                    return HttpResponse('Unsupported file type')
-
-                # Return the file in the appropriate format
-                if ext == '.txt':
-                    response = HttpResponse(file_text, content_type='text/plain')
-                    response['Content-Disposition'] = f'attachment; filename="{filename}'
-                elif ext == '.docx':
-                    response = HttpResponse(file_text, content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-                    response['Content-Disposition'] = f'attachment; filename="{filename}"'
-                else:
-                    return HttpResponse('Unsupported file type')
-
-                return response
-            else:
-                # If no file was uploaded, mask the entered text and display it on the page
+                    return HttpResponse('No file was uploaded.')
+            elif encryption_type == 'text':
                 masked_text = mask_text(text)
+                return render(request, 'masker.html', {'form': form, 'masked_text': masked_text, 'text': text})
+            elif encryption_type == 'decrypt_file':
+                if file:
+                    _, ext = os.path.splitext(file.name)
+                    if ext == '.txt':
+                        file_text = file.read().decode('utf-8')
+                        file_text = unmask_text(file_text)
+                        response = HttpResponse(file_text, content_type='text/plain')
+                        response['Content-Disposition'] = 'attachment; filename="unmasked_file.txt"'
+                    elif ext == '.docx':
+                        doc = docx.Document(file)
+                        for para in doc.paragraphs:
+                            para.text = unmask_text(para.text)
+                        output = io.BytesIO()
+                        doc.save(output)
+                        output.seek(0)
+                        response = HttpResponse(output.read(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                        response['Content-Disposition'] = 'attachment; filename="unmasked_file.docx"'
+                    else:
+                        return HttpResponse('Unsupported file type')
+
+                    return response
+                else:
+                    return HttpResponse('No file was uploaded.')
+            elif encryption_type == 'decrypt_text':
+                print(encryption_type)
+                # If no file was uploaded, mask the entered text and display it on the page
+                masked_text = unmask_text(text)
                 return render(request, 'masker.html', {'form': form, 'masked_text': masked_text, 'text': text})
     else:
         form = MaskerForm()
         return render(request, 'masker.html', {'form': form})
+
     
 def aboutus(request):
     return render(request, 'aboutus.html')
